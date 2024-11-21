da8ff10ca3b6563c09c4d47860b836e6b0e97738
Eg0R98 <stulikove98@gmail.com>
2024-11-21 12:58:57 +0500

    Delete src/main/java/com/stulikov/public_transport_stops_on_spring/experiments directory

--- src/main/java/com/stulikov/public_transport_stops_on_spring/experiments/forecast/Test.java
+++
@@ -1,5 +0,0 @@
-package com.stulikov.public_transport_stops_on_spring.experiments.forecast;
-
-public class Test {
-
-}
--- src/main/java/com/stulikov/public_transport_stops_on_spring/experiments/test/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.stulikov.public_transport_stops_on_spring.experiments.test;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("src/main/java/experiments/test")
-public class ApplicationConfig {
-}
